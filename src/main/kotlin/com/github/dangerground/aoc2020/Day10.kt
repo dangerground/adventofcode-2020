c84653afe8ab02d17023ebd8d12fdc922e38aa8e
Danny Götte <danny.goette@fem.tu-ilmenau.de>
2020-12-10 21:48:31 +0100

    day10 done

--- src/main/kotlin/com/github/dangerground/aoc2020/Day10.kt
+++ src/main/kotlin/com/github/dangerground/aoc2020/Day10.kt
@@ -9,126 +9,51 @@ fun main() {
     println("result part 2: ${process.part2()}")
 }
 
-class Day10(val input: List<Int>) {
+class Day10(adapters: List<Int>) {
 
-    val max = input.max()
+    private val sortedAdapters = mutableListOf<List<Int>>()
+
+    init {
+        val input = adapters.toMutableList()
+        input.add(0)
+
+        var newList1 = mutableListOf<Int>()
+        input.sorted().forEach {
+            if (newList1.isNotEmpty() && newList1.last() + 3 == it) {
+                sortedAdapters.add(newList1)
+                newList1 = mutableListOf()
+            }
+            newList1.add(it)
+        }
+        if (!newList1.isEmpty()) {
+            sortedAdapters.add(newList1)
+        }
+    }
 
     fun part1(): Int {
-        var currentJolt = 0
-        var oneJoltDiff = 0
-        var threeJoltDiff = 0
+        val oneJoltDiff = sortedAdapters.map { it.size-1 }.sum()
+        val threeJoltDiff = sortedAdapters.size
 
-        while (true) {
-            when {
-                input.contains(currentJolt + 1) -> {
-                    currentJolt += 1
-                    oneJoltDiff++
-                }
-                input.contains(currentJolt + 2) -> {
-                    currentJolt += 2
-                }
-                input.contains(currentJolt + 3) -> {
-                    currentJolt += 3
-                    threeJoltDiff++
-                }
-                else -> {
-                    currentJolt += 3
-                    threeJoltDiff++
-                    break
-                }
-            }
-        }
-
-        println("$oneJoltDiff * $threeJoltDiff")
         return oneJoltDiff * threeJoltDiff;
     }
 
-
     fun part2(): Long {
-        return prcountRecursive(1) + prcountRecursive(2) + prcountRecursive(3)
-    }
-    fun prcountRecursive(c: Int): Long {
-//        println("----$c")
-        return countRecursive(c)
+        return sortedAdapters
+            .map { permutations(it) }
+            .reduce { i1, i2 -> i1 * i2 }
     }
 
-    fun countRecursive(current: Int): Long {
-        if (current == max) {
-//            print("$current!")
-            return 1
+    private fun permutations(i: List<Int>): Long {
+        // copied values from recursive function for all possible ways
+        // TODO find a reprentative mathematical way of computation
+        return when (i.size) {
+            5 -> 7
+            4 -> 4
+            3 -> 2
+            2 -> 1
+            1 -> 1
+            else -> 0
         }
-        if (!input.contains(current)) {
-//            println()
-            return 0
-        }
-//        print("$current, ")
-
-        var result = 0L
-        result += countRecursive(current + 1)
-        result += countRecursive(current + 2)
-        result += countRecursive(current + 3)
-        //println("[$current] = $result")
-
-        return result
-    }
-
-
-    fun part2NonRecursive(): Long {
-        val sorted = input.sorted().toMutableList()
-
-        val newList = mutableListOf<Int>()
-
-        sorted.forEach {
-            println("test $it -> ${sorted.contains(it-1)}, ${sorted.contains(it-2)}, ${sorted.contains(it-3)}")
-            if (!sorted.contains(it-1)
-                    xor !sorted.contains(it+1)) {
-                newList.add(it)
-            }
-        }
-        println(newList)
-
-//        sorted.removeIf {!sorted.contains(it-1) && !sorted.contains(it-2) && sorted.contains(it-3)}
-        println(sorted.subList(1, sorted.size-1))
-
-
-        return countNonRecursive(1) + countNonRecursive(2) + countNonRecursive(3)
-    }
-
-    fun countNonRecursive(start: Int = 1): Long {
-        if (input[start-1] > 3) {
-            return 0
-        }
-
-        var sum = 1L
-        var i = start - 1
-        while(i < input.size) {
-            val element = input[i]
-            print("$element")
-            var step = 1
-            val thisSum = when {
-                input.contains(element + 1) && input.contains(element + 2) && input.contains(element + 3) -> {
-                    print("(3)")
-                    step = 3
-                    4
-                }
-                input.contains(element + 1) && input.contains(element + 2) -> {
-                    print("(2)")
-                    step = 2
-                    2
-                }
-                input.contains(element + 1) || input.contains(element + 3) -> {
-                    print("(1)")
-                    0
-                }
-                else -> {
-                    1
-                }
-            }
-            print(", ")
-            sum += thisSum
-            i += step
-        }
-        return sum
     }
 }
 
