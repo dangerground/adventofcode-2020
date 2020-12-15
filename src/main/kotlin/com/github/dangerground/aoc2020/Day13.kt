768f05d207414f71394c084a05d55ec2942cb01b
Danny Götte <danny.goette@fem.tu-ilmenau.de>
2020-12-15 08:33:06 +0100

    day 13 still incomplete

--- src/main/kotlin/com/github/dangerground/aoc2020/Day13.kt
+++ src/main/kotlin/com/github/dangerground/aoc2020/Day13.kt
@@ -31,33 +31,68 @@ class Day13(val input: List<String>) {
         return lineDiff.first * lineDiff.second
     }
 
-    fun part2(): Long {
+    fun part2Linear(): Long {
 
         val all = busLines.mapIndexed { idx, it -> Pair(idx, it) }
-        val diff = all.maxByOrNull { it.second }!!
 
-        val test = all.map { Pair(it.first - diff.first, it.second) }.filter { it.second != 0L }
+        val test = all.filter { it.second != 0L }
+        val diff = test.first().second
 
         val begin = System.currentTimeMillis()
-        var i = diff.first.toLong()
-        while (i < 5000) {
+        var i = diff * diff
+        do {
 
             if (!test.map {
-                    println("($i + ${it.first})  % ${it.second}")
+                    //    println("($i + ${it.first}) % ${it.second}")
                     (i + it.first) % it.second == 0L
+                }.contains(false)) {
+                println((System.currentTimeMillis() - begin))
+                return i * diff
+            }
+            i += diff
+        } while (true)
+        return -1
+    }
+
+    fun part2(): Long {
+
+        var all = busLines.mapIndexed { idx, it -> Pair(idx, it) }
+        val indices = all.map { it.first }
+        var steps = all.map { it.second }
+        var values = all.map { it.second }
+
+        val testable = all.filter { it.second != 0L }
+        val diff = testable.first().second
+
+        val begin = System.currentTimeMillis()
+
+        var i = 0L
+        while (i < 15) {
+            val max = values.maxOf { it }
+            values = indices.map {
+                if (values[it] == 0L) {
+                    return@map 0
+                }
+                if (values[it] == max) {
+                    println("m $max")
+                    return@map max
+                }
+                val tmp = (values[it]) / steps[it]
+                println("$tmp = $max / ${steps[it]} ((${values[it]}) / ${steps[it]})")
+                return@map (tmp+1) * steps[it]
+            }
+            println("$max - $values")
+            i = values.first()
+            if (!indices.filter { values[it] != 0L }.map {
+                    println("(($i + ${it}) == ${values[it]}")
+                    (i + it) == values[it]
                 }.contains(false)) {
                 println((System.currentTimeMillis() - begin))
                 return i
             }
-            i += diff.second
+
         }
         return -1
-/*
-        println(busLines.size)
-        println(list)
-
-        return -1
-        */
     }
 
 }
